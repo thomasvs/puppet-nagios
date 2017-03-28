90515e23eaa8684effadd463daf1c937a2404999
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2017-03-28 01:52:02 +0000

    allow customizing service description

--- manifests/check/http.pp
+++ manifests/check/http.pp
@@ -1,6 +1,7 @@
 define nagios::check::http (
   $ensure                   = $::nagios_check_http_ensure,
   $args,
+  $service_description,
   $host_name                = $::nagios::client::host_name,
   $servicegroups            = $::nagios_check_http_servicegroups,
   $check_period             = $::nagios_check_http_check_period,
@@ -15,6 +16,12 @@ define nagios::check::http (
     Package <| tag == 'nagios-plugins-http' |>
   }
 
+  if $service_description == '' {
+    $real_service_description = "http_${title}"
+  } else {
+    $real_service_description = $service_description
+  }
+
   nagios::client::nrpe_file { "check_http_${title}":
     ensure => $ensure,
     args   => $args,
@@ -24,7 +31,7 @@ define nagios::check::http (
   nagios::service { "check_http_${title}_${::nagios::client::host_name}":
     ensure              => $ensure,
     check_command       => "check_nrpe_http_${title}",
-    service_description => "http_${title}",
+    service_description => $real_service_description,
     host_name                => $host_name,
     servicegroups            => $servicegroups,
     check_period             => $check_period,
